5a090c2826f0c77eea7d0bc89b6f67cf8b7c54c2
AceBurgundy <samadriansabalo99@gmail.com>
2022-01-21 15:01:03 +0800

    Create the inputTaskHandler and useState hook

---
+++ src/components/TasksList.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+import Task from "./Task";
+
+function TasksList()
+{
+  const taskItemList = [
+    "Follow Edukasyon.ph on Facebook.",
+    "Follow AWS Siklab Pilipinas on Facebook.",
+    "Follow Zuitt Coding Bootcamp on Facebook."
+  ];
+
+  const [taskValue, setTaskValue] = useState("Just another task");
+ 
+  return (
+    <div>
+    <input className="task-input" placeholder="Create a new task" onChange={(e) => console.log(e.target.value)}/>
+    <ul>
+      {taskItemList.map((task, index) => {
+        return <Task key={index} taskName = {task} />
+      })}
+    </ul>
+    </div>
+  );
+}
+
+export default TasksList;
--- src/index.js
+++ src/index.js
@@ -2,36 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+import TasksList from "./components/TasksList";
+
 //My first component
-function Task(props)
-{
-  //console.log(props);
-
-  return (
-      <li className="tasks-item">{props.taskName}</li>
-    );
-}
-
-function Tasklist()
-{
-  const taskItemList = [
-    "Follow Edukasyon.ph on Facebook.",
-    "Follow AWS Siklab Pilipinas on Facebook.",
-    "Follow Zuitt Coding Bootcamp on Facebook."
-  ];
-
-  return (
-    <div>
-    <input className="task-input" />
-    <ul>
-      {taskItemList.map((task, index) => {
-        return <Task key={index} taskName = {task} />
-      })}
-    </ul>
-    </div>
-  );
-}
-
 function App()
 {
   const paraStyle = {
@@ -44,7 +17,7 @@ function App()
       <header>
         <h1>My First to do list app</h1>
         <p style={paraStyle}>The most simple amazing todo-list React app.</p>
-        <Tasklist/>
+        <TasksList/>
        </header>
     </div>
   </div>

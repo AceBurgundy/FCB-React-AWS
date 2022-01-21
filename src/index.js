fe276b3f7cd5723dd54c8cdbf92b7dec6cafcd6d
AceBurgundy <samadriansabalo99@gmail.com>
2022-01-21 14:06:49 +0800

    Add input box

--- src/index.js
+++ src/index.js
@@ -14,12 +14,21 @@ function Task(props)
 
 function Tasklist()
 {
+  const taskItemList = [
+    "Follow Edukasyon.ph on Facebook.",
+    "Follow AWS Siklab Pilipinas on Facebook.",
+    "Follow Zuitt Coding Bootcamp on Facebook."
+  ];
+
   return (
+    <div>
+    <input className="task-input" />
     <ul>
-      <Task taskName="Follow Edukasyon.ph on Facebook." />
-      <Task taskName="Follow AWS Siklab Pilipinas on Facebook." />
-      <Task taskName="Follow Zuitt Coding Bootcamp on Facebook." />
+      {taskItemList.map((task, index) => {
+        return <Task key={index} taskName = {task} />
+      })}
     </ul>
+    </div>
   );
 }
 
